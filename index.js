a9cb52864f60b5276063b486f4c9c4daf5f9de9b
KyongEE <nyyk0212@gmail.com>
2022-01-07 10:54:15 +0900

    last commit

--- index.js
+++ index.js
@@ -3,16 +3,15 @@
 var express = require('express');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
 var app = express();
 
 // DB setting
 mongoose.connect(process.env.MONGO_DB);
 var db = mongoose.connection;
-//3
 db.once('open', function(){
   console.log('DB connected');
 });
-//4
 db.on('error', function(err){
   console.log('DB ERROR : ', err);
 });
@@ -22,41 +21,11 @@ app.set('view engine', 'ejs');
 app.use(express.static(__dirname+'/public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
-
-//DB schema
-var contactSchema = mongoose.Schema({
-  name:{type:String, required:true, unique:true},
-  email:{type:String},
-  phone:{type:String}
-});
-var Contact = mongoose.model('contact', contactSchema);
+app.use(methodOverride('_method'));
 
 //Routes
-//Home
-app.get('/', function(req, res) {
-  res.redirect('/contacts');
-});
-
-//Contacts - index
-app.get('/contacts', function(req, res){
-  Contact.find({}, function(err, contacts){
-    if(err) return res.json(err);
-    res.render('contacts/index', {contacts:contacts});
-  });
-});
-
-// Contacts - New
-app.get('/contacts/new', function(req, res){
-  res.render('contacts/new');
-});
-
-// Contacts - create
-app.post('/contacts', function(req, res){
-  Contact.create(req.body, function(err, contact){
-    if(err) return res.json(err);
-    res.redirect('/contacts');
-  });
-});
+app.use('/', require('./routes/home'));
+app.use('/contacts', require('./routes/contacts'));
 
 // Port setting
 var port = 3000;
